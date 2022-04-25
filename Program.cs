a26dfd1e1754782a7bd28c38acdf08fd00308d43
aleksalens <103563991+aleksalens@users.noreply.github.com>
2022-04-25 13:30:41 +0200

    Add files via upload
    Added Zahtev 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace gameshop
+{
+    class Program
+    {
+        static void Main()
+        {
+            double tax;
+
+            Proizvod LegoBlokovi = new Proizvod("Friends Forest House", 41679, 20.25);
+
+
+            Console.WriteLine("Za izlazak iz aplikacije unesite exit!");
+
+            while (true)
+            {
+                Console.WriteLine("\nPDV u procentima?");
+                string input = Console.ReadLine();
+                if (input == "exit") break;
+                if (double.TryParse(input, out tax) && tax > 0)
+                {
+                 
+                    Console.WriteLine("Cena " + LegoBlokovi.GetPrice() + " din pre poreza i "
+                        + Math.Round(LegoBlokovi.GetPrice() * (1 + tax / 100), 2) + " din nakon " + tax + "% poreza.");
+
+                }
+                else
+                {
+                    Console.WriteLine("Pogresan unos! Pokusajte ponovo!");
+                    continue;
+                }
+            }
+        }
+    }
+}
